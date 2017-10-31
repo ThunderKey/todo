07f21e94c5fcab0ef6eebcb46784d55227364e26
Nicolas Ganz <nicolas@keltec.ch>
2017-10-31 15:23:54 +0100

    improved the layouts of the todo lists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
     end
     "uk-alert-#{cls}"
   end
+
+  def form_field form, key, label_config: {}, &block
+    content_tag(:div) do
+      errors = form.object.errors[key]
+      concat form.label(key, {:class => 'uk-form-label'}.merge(label_config))
+      concat(content_tag(:div, class: 'uk-form-controls') do
+        block.call key, ('uk-form-danger' if errors.any?), form.object.class.human_attribute_name(key)
+        if errors.any?
+          concat content_tag(:span, errors.join(', '), class: 'uk-text-danger')
+        end
+      end)
+    end
+  end
 end
