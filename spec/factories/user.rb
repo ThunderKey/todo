d01f9da68fbdd47439fe9738d6dce10c48ffcec1
Nicolas Ganz <nicolas@keltec.ch>
2017-11-07 11:34:39 +0100

    added default time_zone to the user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:email) {|i| "hans#{i}.muster#{i}@test.ch" }
     sequence(:firstname) {|i| "Hans #{i}" }
     sequence(:lastname) {|i| "Muster #{i}" }
+    time_zone 'Berlin'
   end
 end
