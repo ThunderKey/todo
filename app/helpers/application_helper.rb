dc47a8bb4ecc53e777f99acd821cd0beac38a520
Nicolas Ganz <nicolas@keltec.ch>
2017-11-07 17:42:33 +0100

    improved the layout for larger screens

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,19 @@ module ApplicationHelper
       end)
     end
   end
+
+  def grouped_column_classes count
+    classes = 'uk-child-width-1-1'
+    return classes if count < 2
+    classes = "#{classes} uk-child-width-1-2@s"
+    return classes if count < 4
+    classes = "#{classes} uk-child-width-1-4@m"
+  end
+
+  def grouped_sub_column_classes count
+    classes = 'uk-child-width-1-1'
+    return "#{classes} uk-child-width-1-2@xl" if count >= 4
+    return "#{classes} uk-child-width-1-2@l uk-child-width-1-4@xl" if count >= 2
+    "#{classes} uk-child-width-1-2@2 uk-child-width-1-4@m uk-child-width-1-5@l uk-child-width-1-6@xl"
+  end
 end
