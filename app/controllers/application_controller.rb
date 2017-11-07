7dbc5a4d8d47deececf90ba89112aada63475a76
Nicolas Ganz <nicolas@keltec.ch>
2017-11-07 12:55:55 +0100

    redirect to dashboard_path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   around_action :set_time_zone, if: :current_user
 
-  def new_session_path(_scope)
+  protected
+
+  def new_session_path _scope
     new_user_session_path
+  end
+
+  def after_sign_in_path_for resource
+    request.env['omniauth.origin'] || stored_location_for(resource) || dashboard_path
   end
 
   private
